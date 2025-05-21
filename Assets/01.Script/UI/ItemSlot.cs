8199aceaf4e12fa1c74791bdbdbe72365a6d27f6
garamzui <forsave121@gmail.com>
2025-05-22 02:14:55 +0900

    아이템 획득 및 퀵슬롯에 저장

--- Assets/01.Script/UI/ItemSlot.cs
+++ Assets/01.Script/UI/ItemSlot.cs
@@ -7,47 +7,27 @@ using UnityEngine.UI;
 public class ItemSlot : MonoBehaviour
 {
     public ItemData item;
+    public RawImage icon;
+    public TextMeshProUGUI itemNameText;
+    public TextMeshProUGUI slotNumber;
 
-    public Button button;
-    public Image icon;
-    public TextMeshProUGUI quantityText;
-    private Outline outline;
-
+    public UIQuickBoard UQB;
     /*public UIInventory inventory;*/
 
     public int index;
     public bool equipped;
     public int quantity;
-    private void Awake()
-    {
-        outline = GetComponent<Outline>();
-    }
-
-    private void OnEnable()
-    {
-        outline.enabled = equipped;
-    }
     public void Set()
     { 
         icon.gameObject.SetActive(true);
-        icon.sprite = item.icon;
-        quantityText.text = quantity > 1? quantity.ToString() : string.Empty;
-
-        if (outline != null) 
-        {
-            outline.enabled = equipped;
-        }
+        /*icon.sprite = item.icon;*/
+       itemNameText.text = item.displayName;
     }
 
     public void Clear()
     {
         item = null;
         icon.gameObject.SetActive(false);
-        quantityText.text = string.Empty;
-    }
-
-    public void OnClickButton()
-    { 
-        /*inventory.SelectItem(index);*/
+        itemNameText.text = string.Empty;
     }
 }
---
+++ Assets/01.Script/UI/UIQuickBoard.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class UIQuickBoard : MonoBehaviour
+{
+    public ItemSlot[] slots;
+
+   
+    public Transform slotPanel;
+    
+    
+    private PlayerController controller;
+    private PlayerCondition condition;
+
+    ItemData selectedItem;
+    int selectedItemIndex = 0;
+
+    int curEquipIndex;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = PlayerManager.Instance.Player.controller;
+        condition = PlayerManager.Instance.Player.condition;
+       
+
+        PlayerManager.Instance.Player.addItem += AddItem;
+        slots = new ItemSlot[slotPanel.childCount];
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>();
+            slots[i].index = i;
+            slots[i].slotNumber.text = (i+1).ToString();
+            slots[i].UQB = this;
+        }
+        
+    }
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item != null)
+            {
+                slots[i].Set();
+            }
+            else
+            {
+                slots[i].Clear();
+            }
+        }
+    }
+    void AddItem()
+    {
+        ItemData data = PlayerManager.Instance.Player.itemData;
+
+       
+        ItemSlot emptySlot = GetEmptySlot();
+      
+
+        if (emptySlot != null)
+        {
+            emptySlot.item = data;
+           
+            UpdateUI();
+            PlayerManager.Instance.Player.itemData = null;
+            return;
+        }
+      
+      
+        PlayerManager.Instance.Player.itemData = null;
+        
+
+    }
+
+    ItemSlot GetEmptySlot()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item == null)
+            {
+                return slots[i];
+            }
+        }
+        return null;
+    }
+}
+
